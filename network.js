17ef4beeee7154692780edcae239284ab9a3425a
ProfessionalNoobfr <evanviero@gmail.com>
2026-03-27 09:34:35 +1000

    Add files via upload

---
+++ network.js
@@ -0,0 +1,65 @@
+import { io } from "https://cdn.socket.io/4.8.3/socket.io.esm.min.js";
+const socket = io("http://localhost:3001")
+var elementselected = ""
+
+document.addEventListener("mousemove",function(event){
+    const clientx = event.clientX + document.getElementById("container").scrollLeft
+    const clienty = event.clientY + document.getElementById("container").scrollTop
+
+    const element = document.getElementById(elementselected)
+    if (element != undefined){
+        element.style.position = "absolute"
+        element.style.transform = "none"
+        element.style.top = clienty + "px"
+        element.style.left = clientx + "px"
+        socket.emit("updElement", element.id, clientx, clienty, element.innerHTML)
+    }
+})
+
+document.addEventListener("click", function(event){
+    console.log(event.target.classList)
+    if (elementselected == event.target.id || event.target.classList.contains("nondraggable")){
+        elementselected = ""
+        
+    }else{
+        elementselected = event.target.id
+    }
+})
+
+socket.on("onloadelements", (data) =>{
+    console.log(data)
+    const element = document.getElementById(data[0])
+    const results = document.getElementById("results")
+    if (element != undefined){
+        element.style.transform = "none"
+        element.style.top = data[1][1] + "px"
+        element.style.left = data[1][0] + "px"
+    }else{
+        const elementnew = document.createElement("div")
+        elementnew.style.transform = "none"
+        elementnew.id = data[0]
+        elementnew.style.top = data[1][1] + "px"
+        elementnew.style.left = data[1][0] + "px"
+        elementnew.innerHTML = data[1][2]
+        elementnew.style.position = "relative"
+        results.append(elementnew)
+    }
+})
+
+socket.on("newsearch", (element) =>{
+    document.getElementById("results").innerHTML = ""
+})
+
+socket.on("syncelement", (elementid, posx, posy, text) =>{
+    const element = document.getElementById(elementid)
+    const results = document.getElementById("results")
+    if (element != undefined){
+        element.style.transform = "none"
+        element.style.top = posy + "px"
+        element.style.left = posx + "px"
+        element.style.position = "absolute"
+        if (text){
+            element.innerHTML = text
+        }
+    }
+})
---
+++ search.js
@@ -0,0 +1,91 @@
+var searched = false
+import { io } from "https://cdn.socket.io/4.8.3/socket.io.esm.min.js";
+const socket = io("http://localhost:3001")
+
+document.getElementById("searchbtn").addEventListener("click", function(event){
+    const query = document.getElementById("searchbox")
+    var value = query.value
+    searched = true
+    if (query){
+        pullresults(query.value)
+    }
+})
+
+document.getElementById("clearresults").addEventListener("click", function(event){
+    const results = document.getElementById("results")   
+    results.innerHTML = ""
+    socket.emit("newsearch")
+})
+
+document.addEventListener("click", function(event){
+    if (event.target){
+        if (event.target.className == "query"){
+            search(event.target.innerHTML)
+        }
+
+    }
+})
+
+const observer = new IntersectionObserver((entries) => {
+    for (let entry of entries){
+        if (entry.isIntersecting) {
+            entry.target.classList.add('enter');
+        }
+    }
+});
+
+async function search(query) {
+    const url = new URL(`https://en.wikipedia.org/w/api.php`)
+    url.searchParams.set("origin", "*")
+    url.searchParams.set("format", "json")
+    url.searchParams.set("action", "parse")
+    url.searchParams.set("prop", "text")
+    url.searchParams.set("page", query)
+    const results = document.getElementById("results")   
+    results.innerHTML = ""
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.parse.text["*"])
+    console.log(data)
+    display(data.parse.text["*"])
+}
+
+async function pullresults(query) {
+    const url = new URL(`https://en.wikipedia.org/w/api.php`)
+    url.searchParams.set("origin", "*")
+    url.searchParams.set("format", "json")
+    url.searchParams.set("action", "query")
+    url.searchParams.set("list", "search")
+    url.searchParams.set("srsearch", query)
+
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+    displayreults(data.query.search)
+}
+
+async function display(table) {
+    console.log("hi")
+    const results = document.getElementById("results")
+    results.innerHTML = ""
+    var newelement = document.createElement("div")
+    newelement.innerHTML = table
+    newelement.className = "result generaleffect"
+    observer.observe(newelement)
+    results.append(newelement)
+}
+
+async function displayreults(table) {
+    const results = document.getElementById("results")
+    results.innerHTML = ""
+    for (let result of table){
+        console.log(result)
+        var newelement = document.createElement("div")
+        newelement.innerHTML = `<h1>${result.title}</h1><p>${result.snippet}</p>`
+        newelement.className = "result generaleffect"
+        newelement.id = "queryresult" + result.pageid
+        observer.observe(newelement)
+        results.append(newelement)
+        socket.emit("syncresults", newelement.id, newelement.innerHTML, newelement.tagName, newelement.style.left,newelement.style.top)
+    }
+}
