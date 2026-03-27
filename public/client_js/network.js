88c542981f5f0c3803d19154d925db2700de6b54
ProfessionalNoobfr <evanviero@gmail.com>
2026-03-27 11:21:04 +1000

    Update network.js

--- public/client_js/network.js
+++ public/client_js/network.js
@@ -1,5 +1,5 @@
 import { io } from "https://cdn.socket.io/4.8.3/socket.io.esm.min.js";
-const socket = io("http://localhost:3001")
+const socket = io("https://wikipedia-ripoff.onrender.com")
 var elementselected = ""
 
 document.addEventListener("mousemove",function(event){
